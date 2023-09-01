19a63ab00fa36a72f6db92d8d2836d40dce2d85f
Aisha Ramiro <aishamontealto@gmail.com>
2023-09-01 19:31:30 -0300

    update pet

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -17,5 +17,9 @@ router.get('/mypets', checkToken, PetController.getAllUserPets)
 router.get('/myadoptions', checkToken, PetController.getAllUserAdoptions)
 router.get('/:id', PetController.getPetById)
 router.delete('/:id', checkToken, PetController.removePetById)
+router.patch('/:id', 
+checkToken,
+imageUpload.array('images'),
+PetController.updatePet )
 
 module.exports = router
